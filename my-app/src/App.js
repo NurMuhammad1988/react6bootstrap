8cbd8f434cc11384c2016c1e9985d07d66a2ce84
NurMuhammad1988 <nurikyorov88@gmail.com>
2023-05-11 18:01:31 +0500

    bootstrap react

---
+++ my-app/src/App.js
@@ -0,0 +1,29 @@
+
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import React_bootstrap from './Components/React_bootstrap';
+
+
+
+
+class App extends Component {
+
+
+
+  render(props) {
+    return (
+
+      <div>
+
+      <React_bootstrap/>
+        
+      </div>
+
+    )
+  }
+}
+
+
+export default App;
+
